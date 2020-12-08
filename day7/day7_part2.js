90e3bb0605317e40db18be277a68dcbafc1fcc5a
zachary.heath <z_heath3@yahoo.com>
2020-12-08 11:43:40 -0800

    cleanedup part 2 day 7

--- day7/day7_part2.js
+++ day7/day7_part2.js
@@ -1,13 +1,12 @@
 const print = require('../helpers/print');
 const getFileData = require('../helpers/getFileData');
 const parseRule = require('../helpers/parseRule');
-const _ = require("lodash");
+const rules = getFileData('/day7/day7_test2.txt','\n');
 
-const rules = getFileData('/day7/day7_real.txt','\n');
-const parsedRules = rules.map(rule => parseRule(rule));
 const bagMap = {};
-parsedRules.forEach(rule => bagMap[rule.parent] = rule.children);
-//console.log(bagMap);
+rules
+    .map(rule => parseRule(rule))
+    .forEach(rule => bagMap[rule.parent] = rule.children);
 
 function crawlTree(parent, flowLog) {
     const currChildren = bagMap[parent.name];
@@ -15,18 +14,14 @@ function crawlTree(parent, flowLog) {
         flowLog.push({bag: parent.name, value: parent.value});
         return parseInt(parent.value);
     }
-    const childResult = currChildren.map(child => {   
-        return parent.value * crawlTree(child, flowLog);;
-    });
-    //console.log(`Child result for ${parent.name}: ${childResult}. Adding to ${parent.value}`);
-    //const parentResult = parent.value == 0 ? results.reduce((acc, x) => acc + x, 0) : parent.value + childResult;
-    const parentResult = parent.value + childResult.reduce((acc, n) => acc + n, 0);
-    flowLog.push({bag: parent.name, value: parentResult});
+    const result = parent.value + currChildren
+                                        .map(child => parent.value * crawlTree(child, flowLog))
+                                        .reduce((acc, n) => acc + n, 0);
+    flowLog.push({bag: parent.name, value: result});
     console.log(flowLog);
-    return parentResult;
+    return result;
 }
 
-//f(bag) = bagValue + forEachChild(f(child));
-
+//subtract one from final result to pull the shiny gold bag off the total.
 console.log(crawlTree({value: 1, name: 'shiny gold'}, [], [])-1);
 
